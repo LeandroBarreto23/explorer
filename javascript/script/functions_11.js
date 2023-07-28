ec89f887c967896d4f2a323498247b9a3330d62e
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-28 13:53:18 -0300

    add functions_11

---
+++ javascript/script/functions_11.js
@@ -0,0 +1,10 @@
+// Manipulando Strings e Números
+
+// Transformar
+
+// Um numero quebrado com 2 casas decimais
+let number = 3.1415
+console.log(number.toFixed(2))
+
+// Trocar ponto por vírgula
+console.log(number.toFixed(2).replace(".", ","))
