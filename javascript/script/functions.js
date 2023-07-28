e0dd0e709e8df7362c3705bce25f5c77f8173e95
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-28 08:30:06 -0300

    add functions

---
+++ javascript/script/functions.js
@@ -0,0 +1,13 @@
+// criar um aplicativo de frases motivacionais
+
+// function statement or declaration - declaração da função
+function createPhases() {
+  console.log('Estudar é muito bom')
+  console.log('Paciencia e persistencia')
+  console.log('Revisão é a mãe do aprendizado')
+}
+
+// execute funtion - run, call, invoke (rodar, chamar, invocar)
+createPhases()
+
+console.log('fim do programa')
