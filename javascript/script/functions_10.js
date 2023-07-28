97a769082f8bccae7df38f6013a675e5508446c2
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-28 13:47:27 -0300

    add functions_10

---
+++ javascript/script/functions_10.js
@@ -0,0 +1,11 @@
+// manipulando Strings e Números
+
+// Contar 
+
+// Quantos caracteres tem uma palavra
+let word = "Paralelepipedo"
+console.log(word.length)
+
+// Quantos dígitos tem um número
+let number = 1234
+console.log(String(number).length)
