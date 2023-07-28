2a00ca1796e1a881ac2b4bf281da57d8f440e063
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-28 14:09:03 -0300

    add functions_14

---
+++ javascript/script/functions_14.js
@@ -0,0 +1,7 @@
+// Manipulando Strings
+
+// Verificar se o texto contém a palavra "Amor"
+
+let phrase = "Eu quero viver o amor!"
+console.log(phrase.includes("Amor")) 
+//includes é o que faz a busca da palavra e escrever com maiusculas e minusculas faz total diferenca na procura
