ff68127cd5460ed1a06a4f885f760c84af2301c4
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-28 10:09:42 -0300

    add function_6

--- javascript/script/functions_6.js
+++ javascript/script/functions_6.js
@@ -1,2 +1,13 @@
-// callback function
+// callback function - chamar de volta
 
+function sayMyName(name) {
+  console.log('antes de executar a callback')
+  name()
+  console.log('depois de executar a callback')
+}
+
+sayMyName(
+  () => {
+    console.log('estou em uma callback')
+  }
+)
