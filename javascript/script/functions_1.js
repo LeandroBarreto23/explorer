0cb54d9d9d25e5769ce33efaee32b03e379f4ca5
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-28 09:11:40 -0300

    add function_1

---
+++ javascript/script/functions_1.js
@@ -0,0 +1,17 @@
+// function expression - function anonymous
+
+// parameters (parâmetros)
+const sum = function(number1, number2) {
+  let total = number1 + number2
+  return total
+}
+
+//arguments (argumentos)
+//sum(2, 3) 
+
+let number1 = 20
+let number2 = 33
+
+console.log(`o número 1 é: ${number1}`)
+console.log(`o número 2 é: ${number2}`)
+console.log(`a soma dos dois é ${sum(number1, number2)}`)
