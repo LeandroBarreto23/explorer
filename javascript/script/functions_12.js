45da88e6042986c37552cc7acf608ca3599c4a77
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-28 13:56:25 -0300

    add functions_12

---
+++ javascript/script/functions_12.js
@@ -0,0 +1,11 @@
+// Manipulando Strings e Números
+
+// Transforme:
+
+// letras minúsculas em maiúsculas
+let word = "Programar é muito bacana!"
+console.log(word.toUpperCase())
+
+// letras maiúsculas em minúsculas
+let word2 = "Programar é muito bacana!"
+console.log(word2.toLowerCase())
