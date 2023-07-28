4568d7e5e74eee5277b5afd936ee96a50405d540
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-28 13:36:58 -0300

    add functions_8

---
+++ javascript/script/functions_8.js
@@ -0,0 +1,8 @@
+// Type conversio (typecasting) -> nossa capacidade de converter um tipo de dado para outro tipo
+// Type coersion -> capacidade do JS forçar a conversão de um tipo de dado para outro tipo
+
+//o JS usou o type coersion transformando um number em string.
+console.log('9' + 5) 
+
+//o JS agora usou o typecasting para transformar uma string em number
+console.log(Number('9') + 5)
