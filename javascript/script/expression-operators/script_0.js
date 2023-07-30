ff89a1ab10374c8cc51e87c6fafca252bde5f367
Leandro Barreto de Sousa <leandro.barreto.lb23@gmail.com>
2023-07-29 23:01:26 -0300

    add expression-operator script_0

---
+++ javascript/script/expression-operators/script_0.js
@@ -0,0 +1,13 @@
+// expressoes e operadores
+
+// Expression -> let number (é uma expressão)
+// Operators
+//    Binary = console.log(number + 1) -> é binário pq precisa-se de dois operadores aritméticos para que haja uma soma
+//    Unary = console.log(++number) -> que só tem 1 elemento e auto-incrementa ou decrementa
+//    Ternary = console.log(false ? 'alo' : 'nada')
+
+let number = 1
+
+console.log(++number)
+
+console.log(false ? 'alo' : 'nada')
