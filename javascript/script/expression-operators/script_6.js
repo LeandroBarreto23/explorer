d64cbce67a0663bf79d5d641d904bc871ab15bb4
Leandro Barreto de Sousa <leandro.barreto.lb23@gmail.com>
2023-07-29 23:53:19 -0300

    add script_6

---
+++ javascript/script/expression-operators/script_6.js
@@ -0,0 +1,32 @@
+// Operadores de atribuicao (assignment)
+
+let x
+console.log(x)
+
+// assignment
+x = 1
+console.log(x)
+
+// addition assignment
+x += 2 // x = x + 2
+console.log(x)
+
+// subtraction assignment
+x -= 1 // x = x - 1
+console.log(x)
+
+// multiplication assignment
+x *= 2 // x = x * 2
+console.log(x)
+
+// division assignment
+x /= 2 // x = x / 2
+console.log(x)
+
+// remainder
+x %= 2
+console.log(x)
+
+// exponeatiation
+x **= 2
+console.log(x)
