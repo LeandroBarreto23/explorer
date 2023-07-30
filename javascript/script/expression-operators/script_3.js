6e5a7df86a6b0ec87a333b4e5bfa958187fc2410
Leandro Barreto de Sousa <leandro.barreto.lb23@gmail.com>
2023-07-29 23:25:21 -0300

    add script_3

---
+++ javascript/script/expression-operators/script_3.js
@@ -0,0 +1,29 @@
+// Operadores Aritiméticos
+
+// multiplicação "*"
+console.log(3.2 * 5.5)
+
+// divisão "/"
+console.log(12/2)
+
+// soma "+"
+console.log(34 + 67)
+
+// substração "-"
+console.log(34 - 23)
+
+// resto da divisão (o que sobra depois de dividir) "%"
+let remainder
+remainder = 11 % 4
+console.log(remainder)
+
+// incremento "++"
+let increment = 0
+console.log(++increment)
+
+// decremento "--"
+let decrement = 0
+console.log(--decrement)
+
+// exponencial "**"
+console.log(3 ** 2) //(3 elevado a 2)
