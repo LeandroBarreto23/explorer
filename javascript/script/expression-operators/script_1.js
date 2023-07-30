3273e78fcf027e5a4e961e0a1422e38b82a6d137
Leandro Barreto de Sousa <leandro.barreto.lb23@gmail.com>
2023-07-29 23:08:49 -0300

    add scritp_1

---
+++ javascript/script/expression-operators/script_1.js
@@ -0,0 +1,8 @@
+// new -> left-hand-side expression (criar um novo objeto)
+
+let Name = new String('Leandro')
+Name.surName = 'Barreto'
+let age = new Number(23)
+let date = new Date('2000-02-29')
+
+console.log(Name.surName, age, date)
