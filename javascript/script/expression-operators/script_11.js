f0d9442fc2a83dd7e4487ecfe6cae8ec4e017814
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-31 15:09:26 -0300

    add script_11

---
+++ javascript/script/expression-operators/script_11.js
@@ -0,0 +1,17 @@
+// operator precedence - precedencia de operadores
+
+// grouping                   ( )
+// negação e incremento       ! ++ --
+// multiplicação e divisão    * /
+// adição e subtração         + -
+// relacional                 < <= > >=c
+// igualdade                  == != === !==
+// AND                        &&
+// OR                         ||
+// condicional                ? :
+// assignment (atribuição)    = += -= *= %=
+
+console.log( (2 + 5) * 10)
+
+console.log( 3 > 2 > 1 ) // 3 maior que 2? true. true maior que 1? false
+console.log( 3 > 2 && 2 > 1 ) // 3 maior que 2? true. 2 maior que 1? true. true + true = true
