94dba73f80d0f2c744c128187ca9bc8b746204c3
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-31 13:12:16 -0300

    add script_10

---
+++ javascript/script/expression-operators/script_10.js
@@ -0,0 +1,5 @@
+// type conversion (typecasting) vs type coersion
+
+console.log(Number('9') + 5)
+
+// falsy -> quando um valor é considerado em contextos onde um booleano é obrigatório (condicionais e loops)
