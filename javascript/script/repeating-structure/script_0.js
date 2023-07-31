b909d774ba865996b28e8e50075ede5f7743b9dc
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-31 16:13:41 -0300

    add script_0

---
+++ javascript/script/repeating-structure/script_0.js
@@ -0,0 +1,13 @@
+// estrutura de repeticao
+
+// for - para
+
+// break -> para a execucao do loop
+// continue -> pula a execucao do momento
+
+for(let i = 10; i > 0; i--) {
+  if (i === 5) {
+    continue 
+  }
+  console.log(i)
+}
