7efce2504fe71f6486eda11b21ff80c0e893ea00
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-31 11:35:47 -0300

    edit script_7

--- javascript/script/expression-operators/script_7.js
+++ javascript/script/expression-operators/script_7.js
@@ -1,1 +1,15 @@
 // operadores logicos (logical operators)
+
+// 2 valores booleanos que quando verificados resultará em verdadeiro ou falso
+
+let pao = true
+let queijo = true
+
+// AND "&&" (precisa atender as duas condiçoes para resultar em verdadeiro, caso contrario retornará falso)
+console.log(pao && queijo)
+
+// OR "||" (se atender qualquer uma das duas condições resulta em verdadeiro, se nao cumprir nenhuma resulta em falso)
+console.log(pao || queijo)
+
+// NOT "!" (operador de negacao, troca o valor da variavel, se a variavel for falsa ela vira verdadeira e vice versa)
+console.log(!pao)
