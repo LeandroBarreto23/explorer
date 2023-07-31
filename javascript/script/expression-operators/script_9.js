f1f14e54ceaa49038543f91c29cfe83ab1bbba06
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-07-31 11:49:30 -0300

    add script_9

---
+++ javascript/script/expression-operators/script_9.js
@@ -0,0 +1,11 @@
+// operador de strin (string operator)
+
+// comparison (comparação)
+console.log('a' == 'b')
+
+// concatenation (concatenação)
+let alpha = 'alpha'
+alpha += 'bet'
+console.log(alpha + 345)
+
+// retorna a uniao de duas "strings"
