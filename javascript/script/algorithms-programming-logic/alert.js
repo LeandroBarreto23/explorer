b719f1a14431bc9e65567c4556c41b8f28af0d7d
LeoSousa313 <leandro.barreto.lb23@gmail.com>
2023-08-02 15:59:08 -0300

    add alert

---
+++ javascript/script/algorithms-programming-logic/alert.js
@@ -0,0 +1,11 @@
+// encontre a solução ao problema
+
+// pergunte o nome do usuario e escreva a mensagem: "ola, [nome do usuario]"
+
+let nome = prompt("Qual o seu nome?")
+let age = prompt("Quantos anos você tem?")
+let weight = prompt("Qual é o seu peso?")
+let height = prompt("Qual a sua altura?")
+let country = prompt("Em que país você nasceu?")
+
+alert("Olá leandro, você diz ter " + age + " anos, " + weight + " kg, mede " + height + " de altura e natural do país " + country)
