0e37ea2836e8f01aac519abbd4aa622422d3eb62
Leandro Barreto de Sousa <leandro.barreto.lb23@gmail.com>
2023-08-03 20:15:12 -0300

    update sum

--- javascript/script/algorithms-programming-logic/sum.js
+++ javascript/script/algorithms-programming-logic/sum.js
@@ -6,36 +6,6 @@ alert("Iremos somar dois números.");
 let numberOne = prompt("digite o primeiro número: ");
 let numberTwo = prompt("digite o segundo número: ");
 
-let sum = Number(numberOne) + Number(numberTwo)
+const sum = Number(numberOne) + Number(numberTwo)
 
 alert("O resultado final é: " + sum )
-
-// division
-alert("Agora iremos dividir dois números.")
-
-let numberThree = prompt("digite o primeiro número:")
-let numberFour = prompt("digite o segundo número:")
-
-let div = Number(numberThree) / Number(numberFour)
-
-alert("O resultado final é: " + div)
-
-// multiply
-alert("Agora iremos multiplicar dois números.")
-
-let numberFive = prompt("digite o primeiro número: ")
-let numberSix = prompt("digite o segundo número: ")
-
-let mul = Number(numberFive) * Number(numberSix)
-
-alert("O resultado final é: " + mul)
-
-// subtraction
-alert("Agora iremos subtrair dois número.")
-
-let numberSeven = prompt("digite o primeiro número: ")
-let numberEight = prompt("digite o segundo número: ")
-
-let sub = Number(numberSeven) - Number(numberEight)
-
-prompt("O resultado final é: " + sub)
