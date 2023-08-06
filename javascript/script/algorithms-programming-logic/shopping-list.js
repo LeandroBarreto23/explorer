3b939d6141d768fffc6c36dc954e5473d7244bca
Leandro Barreto de Sousa <leandro.barreto.lb23@gmail.com>
2023-08-06 11:29:17 -0300

    edit shopping list

--- javascript/script/algorithms-programming-logic/shopping-list.js
+++ javascript/script/algorithms-programming-logic/shopping-list.js
@@ -4,4 +4,12 @@
   Após capturar os 10 items, imprima-os, separando por vírgula.
 */
 
+let items = [];
+
 // capturando os 10 items
+for(let i = 0; i < 10; i++) {
+  itemName = prompt('digite o item ' + (i + 1))
+  items[i] = itemName
+}
+
+alert(items)
