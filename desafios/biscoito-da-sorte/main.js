2b3bd88259f4ed4267fcc671abe6c0c954176e87
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2023-08-23 09:53:53 -0300

    add array mensage

--- desafios/biscoito-da-sorte/main.js
+++ desafios/biscoito-da-sorte/main.js
@@ -5,8 +5,17 @@ const screenTwo = document.querySelector(".screenTwo")
 const cookieOpen = document.querySelector("#cookieOpen")
 const openOtherCookie = document.querySelector("#openOtherCookie")
 
+// função callback - adiciona a class 'hide' se nao tiver, remove se tiver
 function toggleScreen() {
     screenOne.classList.toogle('hide')
     screenTwo.classList.toogle('hide')
 }
 
+// array que armazena as frases do biscoito
+const frases = [
+    "Cada linha de código é um passo rumo ao seu objetivo.",
+    "Erros são oportunidades de aprendizado em código.",
+    "Você está construindo o futuro com cada programa.",
+    "A programação é a arte de criar possibilidades.",
+    "Sua mente é a melhor ferramenta de programação."
+]
