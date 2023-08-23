00b0a906596afdb88c18e06e2b77f419412552a4
Leandro Barreto de Sousa <leandro.barreto.lb23@gmail.com>
2023-08-22 22:45:10 -0300

    update jogo da adivinhacao

--- projetos/jogo-adivinhacao/main.js
+++ projetos/jogo-adivinhacao/main.js
@@ -1,7 +1,34 @@
-function handleClick(event) {
-  event.preventDefault()
+// puxando os elementos do HTML para o JS
+const screenOne = document.querySelector(".screenOne")
+const screenTwo = document.querySelector(".screenTwo")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+// função callback
+function handleTryClick(event) {
+  event.preventDefault() //nao faça o padrao desse evento (neste caso, nao envie o formulario)
   
   const inputNumber = document.querySelector("#inputNumber")
-  
-  console.log(inputNumber.value)
+
+  if(Number(inputNumber.value) == randomNumber) {
+    screenOne.classList.add("hide")
+    screenTwo.classList.remove("hide")
+
+    document.querySelector(".screenTwo h2")
+    .innerText = `Você acertou em ${xAttempts} tentativas`
+  }
+
+  inputNumber.value = "" //para resetar o valor do input toda vez q errar o numero correto
+  xAttempts++
 }
+
+// função callback
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function() {
+  screenOne.classList.remove("hide")
+  screenTwo.classList.add("hide")
+  xAttempts = 1
+})
