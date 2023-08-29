6e9999cc9a034367b5e269da1813d45f9861ef9a
Leandro Barreto de Sousa <leandro.barreto.lb23@gmail.com>
2023-08-29 19:54:11 -0300

    Update main.js

--- stage-05/biscoito-da-sorte/main.js
+++ stage-05/biscoito-da-sorte/main.js
@@ -5,17 +5,40 @@ const screenTwo = document.querySelector(".screenTwo")
 const cookieOpen = document.querySelector("#cookieOpen")
 const openOtherCookie = document.querySelector("#openOtherCookie")
 
-// função callback - adiciona a class 'hide' se nao tiver, remove se tiver
+// array que armazena as frases do biscoito
+const phrases = [
+  "Cada linha de código é um passo rumo ao seu objetivo.",
+  "Erros são oportunidades de aprendizado em código.",
+  "Você está construindo o futuro com cada programa.",
+  "A programação é a arte de criar possibilidades.",
+  "Sua mente é a melhor ferramenta de programação."
+]
+
+// execucao de funcao
+openOtherCookie.addEventListener('click', handleTryClick)
+openOtherCookie.addEventListener('click', handleResetClick)
+
+// funções
+// adiciona a class 'hide' se nao tiver, remove se tiver
 function toggleScreen() {
-    screenOne.classList.toogle('hide')
-    screenTwo.classList.toogle('hide')
+  screenTwo.classList.toggle('hide')
+  screenOne.classList.toggle('hide')
 }
 
-// array que armazena as frases do biscoito
-const frases = [
-    "Cada linha de código é um passo rumo ao seu objetivo.",
-    "Erros são oportunidades de aprendizado em código.",
-    "Você está construindo o futuro com cada programa.",
-    "A programação é a arte de criar possibilidades.",
-    "Sua mente é a melhor ferramenta de programação."
-]
+// gerando um numero de posição aleatoria para a frase do array
+function pickPhrases {
+  let allPhrases = phrases.length
+  let randomNumber = Math.floor(Math.random() * allPhrases)
+  screenTwo.querySelector("h2").innerText = `${phrases[randomNumber]}`
+}
+
+// chamando a toggle para realizar a troca da class hide e alternar entre as telas
+function handleResetClick() {
+  toggleScreen()
+}
+
+// 
+function handleTryClick(event) {
+  toggleScreen()
+  pickPhrases() 
+}
