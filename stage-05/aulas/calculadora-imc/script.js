a59d4b4f0b4efc5e6b8d995ad64a628fe21b834a
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2023-09-05 13:55:06 -0300

    finalizando calculadora imc

--- stage-05/aulas/calculadora-imc/script.js
+++ stage-05/aulas/calculadora-imc/script.js
@@ -1,30 +1,44 @@
 // variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
-const inputHeight = document.querySelector('#Height')
+const inputHeight = document.querySelector('#height')
 
-/* 
+// const modalWrapper = document.querySelector('.modal-wrapper')
+// const modalMessage = document.querySelector('.modal .tittle span')
+// const modalBtnClose = document.querySelector('.modal button.close')
 
-3 maneiras de criar e atribuir funcao a um evento
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .tittle span'),
+  buttonClose: document.querySelector('.modal button.close'),
 
-ARROW FUNCTION
-form.onsubmit = (evento) => {
-  mesmo código abaixo
+  open() {
+    Modal.wrapper.classList.add('open')
+  },
+  close() {
+    Modal.wrapper.classList.remove('open')
+  }
 }
 
-
-form.onsubmit = handleSubmit
-function handleSubmit(event) {
-  mesmo código abaixo
-}
-
-*/
-
-form.onsubmit = function(event) {
+form.onsubmit = event => {
   // preventDefault é uma função que remove o padrao do comportamento, neste caso esta fazendo com que o botao do submit nao recarregue a página par enviar os dados
-  event.preventDefault()
-  
+  event.preventDefault()  
   // pegando os valores de peso e altura no input
   const weight = inputWeight.value
   const height = inputHeight.value
+
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+  Modal.message.innerText = message
+  // modalWrapper.classList.add('open')
+  Modal.open()
+}
+
+Modal.buttonClose.onclick = () => {
+  // modalWrapper.classList.remove('open')
+  Modal.close()
+}
+
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
 }
