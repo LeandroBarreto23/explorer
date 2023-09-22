c0a1c51ffc9e5c4fb0f32ddc781b25ef82bd5fa7
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2023-09-21 22:37:10 -0300

    refatorando funcao, aplicando clean code

--- stage-05/aulas/calculadora-imc/js/script.js
+++ stage-05/aulas/calculadora-imc/js/script.js
@@ -1,31 +1,33 @@
 import { Modal } from './modal.js'
 import { alertError } from "./alert-error.js"
-import { IMC, notANumber } from "./utils.js"
+import { calculateIMC, notANumber } from "./utils.js"
 
-// variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
 form.onsubmit = event => {
-  // preventDefault é uma função que remove o padrao do comportamento, neste caso esta fazendo com que o botao do submit nao recarregue a página par enviar os dados
   event.preventDefault()  
-  // pegando os valores de peso e altura no input
+
   const weight = inputWeight.value
   const height = inputHeight.value
   
-  const showAlertError = notANumber(weight) || notANumber(height)
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-  if(showAlertError) {
+  if(weightOrHeightIsNotANumber) {
     alertError.open()
     return;
   }
 
   alertError.close()
 
-  const result = IMC(weight, height)
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
   const message = `Seu IMC é de ${result}`
+
   Modal.message.innerText = message
-  // modalWrapper.classList.add('open')
   Modal.open()
 }
