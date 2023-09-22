000a812acd27e08d82817653eb71264e243a5a9f
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2023-09-21 21:56:14 -0300

    modularizando a aplicacao

--- stage-05/aulas/calculadora-imc/js/script.js
+++ stage-05/aulas/calculadora-imc/js/script.js
@@ -1,24 +1,9 @@
+import { Modal } from './modal.js'
+
 // variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-// const modalWrapper = document.querySelector('.modal-wrapper')
-// const modalMessage = document.querySelector('.modal .tittle span')
-// const modalBtnClose = document.querySelector('.modal button.close')
-
-const Modal = {
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .tittle span'),
-  buttonClose: document.querySelector('.modal button.close'),
-
-  open() {
-    Modal.wrapper.classList.add('open')
-  },
-  close() {
-    Modal.wrapper.classList.remove('open')
-  }
-}
 
 form.onsubmit = event => {
   // preventDefault é uma função que remove o padrao do comportamento, neste caso esta fazendo com que o botao do submit nao recarregue a página par enviar os dados
@@ -32,11 +17,6 @@ form.onsubmit = event => {
   Modal.message.innerText = message
   // modalWrapper.classList.add('open')
   Modal.open()
-}
-
-Modal.buttonClose.onclick = () => {
-  // modalWrapper.classList.remove('open')
-  Modal.close()
 }
 
 function IMC(weight, height) {
