f2311734e843ce216b8cc3ee5a5210e7fe670060
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2023-10-04 06:21:58 -0300

    add actions function

--- stage-05/aulas/focus-timer/js/FocusTimer/events.js
+++ stage-05/aulas/focus-timer/js/FocusTimer/events.js
@@ -1,8 +1,13 @@
 import { controls } from './elements.js'
-
+import * as actions from './actions.js'
 // funcao de observador de click nos botoes
 export function registerControls() {
   controls.addEventListener('click', (event) => {
+    const action = event.target.dataset.action
+    if(typeof actions[action]() != "function") {
+      return
+    }
     
+    actions[action]()
   })
 }
