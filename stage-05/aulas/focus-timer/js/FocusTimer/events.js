425d0f769cbc0e83edba195ad03b5be800b4c513
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2023-10-06 10:33:53 -0300

    add audios, events and timer

--- stage-05/aulas/focus-timer/js/FocusTimer/events.js
+++ stage-05/aulas/focus-timer/js/FocusTimer/events.js
@@ -1,5 +1,9 @@
 import { controls } from './elements.js'
 import * as actions from './actions.js'
+import * as el from './elements.js'
+import { updateDisplay } from './timer.js'
+import state from './state.js'
+
 // funcao de observador de click nos botoes
 export function registerControls() {
   controls.addEventListener('click', (event) => {
@@ -9,5 +13,24 @@ export function registerControls() {
     }
     
     actions[action]()
+  })
+}
+
+export function setMinutes() {
+  el.minutes.addEventListener('focus', () => {
+    el.minutes.textContent = ""
+  })
+  // /\d/ é uma expressão regular que verifica se oq foi digitado é um numero. Se sim, ele retorna "true" e permite digitar numero, sen nao retorna "false" e bloqueia td quer for diferente de numero.
+  el.minutes.onkeypress = (event) => /\d/.test(event.key)
+
+  el.minutes.addEventListener('blur', (event) => {
+    let time = event.currentTarget.textContent
+    time = time > 60 ? 60 : time
+
+    state.minutes = time
+    state.seconds = 0
+
+    updateDisplay()
+    el.minutes.removeAttribute('contenteditable')
   })
 }
--- stage-05/aulas/focus-timer/js/FocusTimer/timer.js
+++ stage-05/aulas/focus-timer/js/FocusTimer/timer.js
@@ -1,5 +1,34 @@
 import state from './state.js'
 import * as el from './elements.js'
+import { reset } from './actions.js'
+import { kitchenTimer } from './sounds.js'
+// recursão - funcao que chama ela mesma em algum momento, mas precisa ter algum momento onde ela para se nao pode haver problemas.
+// setTimeout() funcao que executa uma funcao depois de alguns milisegundos.
+export function countdown() {
+  if(!state.isRunning) {
+    return
+  }
+
+  let minutes = Number(el.minutes.textContent)
+  let seconds = Number(el.seconds.textContent)
+
+  seconds--
+
+  if (seconds < 0) {
+    seconds = 59
+    minutes--
+  }
+
+  if (minutes < 0) {
+    reset()
+    kitchenTimer.play()
+    return
+  }
+
+  updateDisplay(minutes, seconds)
+
+  setTimeout(() => countdown(), 1000)
+}
 
 export function updateDisplay(minutes, seconds) {
   minutes = minutes ?? state.minutes 
