3d68228a4ad66f635143c20925df9288af096fa6
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2024-02-27 07:41:45 -0300

    Static methods and promises

--- stage-06/aulas/github_favorites/js/Favorites.js
+++ stage-06/aulas/github_favorites/js/Favorites.js
@@ -1,9 +1,31 @@
+export class GithubUser {
+  static search(username) {
+    // busca na api os dados em string
+    const endpoint = `https://api.github.com/users/${username}`
+
+    //no fim das contas o fetch me retorna os dados
+    return fetch(endpoint)
+    // QUANDO esse dado vier, transformo ele em JSON
+    .then(data => data.json())
+    // Pego somente os dados que eu quero fazendo desestruturação de objeto
+    .then(({ login, name, public_repos, followers}) => ({
+      login,
+      name,
+      public_repos,
+      followers 
+    }))
+  }
+}
+
+
 // classe que vai conter a logica dos dados
 // como os dados serão estruturados
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
+
+    GithubUser.search('leosousa23').then(user => console.log(user))
   }
 
   // crio um array de objetos com os dados de cada usuario
