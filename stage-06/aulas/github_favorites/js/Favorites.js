aff9a66c69b4ee98067338e01877c71c70e2a8d5
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2024-02-27 00:19:33 -0300

    Creating HTML with JavaScript

--- stage-06/aulas/github_favorites/js/Favorites.js
+++ stage-06/aulas/github_favorites/js/Favorites.js
@@ -3,10 +3,27 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
+    this.load()
+  }
+
+  // crio um array de objetos com os dados de cada usuario
+  load() {
+    this.entries = [
+      {
+      login: 'maykbrito',
+      name: 'Mayk Brito',
+      public_repos: '76',
+      followers: '120000'
+      },
+      {
+      login: 'diego3g',
+      name: 'Diego Fernandes',
+      public_repos: '76',
+      followers: '120000'
+      }
+    ]
   }
 }
-
-
 
 // classe que vai criar a visualizacao e eventos do HTML
 export class FavortesView extends Favorites {
@@ -14,19 +31,53 @@ export class FavortesView extends Favorites {
   constructor(root) {
     super(root)
 
+  // localizo no #app o tbody dentro da table
+  this.tbody = this.root.querySelector('table tbody')
+
     this.update()
   }
 
   update() {
     this.removeAllTr()
+
+    //para cada usuario vou rodar uma funcao
+    this.entries.forEach(user => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+      <td class="user">
+        <img src="https://github.com/diego3g.png" alt="Imagem de maykbrito">
+        <a href="https://github.com/diego3g" target="_blank">
+          <p>Diego Fernandes</p>
+          <span>diego3g</span>
+        </a>
+      </td>
+      <td class="repositories">48</td>
+      <td class="followers">22503</td>
+      <td>
+        <button class="remove">&times;</button>
+      </td>
+    `
+
+    return tr
   }
 
   // funcao para remover as linhas da tablea
   removeAllTr() {
-    // localizo no #app o tbody dentro da table
-    const tbody = this.root.querySelector('table tbody')
     // pego todos os tr, para cada tr, pegue o tr e remova
-    tbody.querySelectorAll('tr').forEach((tr) => {
+    this.tbody.querySelectorAll('tr').forEach((tr) => {
       tr.remove()
     });
   }
