24f013eb9169bd0e8aaf4cf0fea3cb45e422e111
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2024-03-16 14:48:10 -0300

    add function delete user

--- stage-06/gitfav/js/Favorites.js
+++ stage-06/gitfav/js/Favorites.js
@@ -24,6 +24,14 @@ export class Favorites {
       }
     ]
   }
+
+  delete(user) {
+    const filteredEntries = this.entries
+    .filter(entry => entry.login !== user.login)
+
+    this.entries = filteredEntries
+    this.update()
+  }
 }
 
 // criar a visualizacao e eventos do HTML
@@ -46,6 +54,13 @@ export class FavoritesView extends Favorites {
       row.querySelector('.user span').textContent = `/${user.login}`
       row.querySelector('.public_repos').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.btn-remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk) {
+          this.delete(user)
+        }
+      }
 
       this.tbody.append(row)
     })
